742315b0718140aeecf5a2ee0dd5ba8a3cd3b203
Johannes Koch <jokoch@pik-potsdam.de>
2021-06-22 19:45:39 +0200

    Add place holders for SDP scenarios

--- R/calcCapital.R
+++ R/calcCapital.R
@@ -34,11 +34,13 @@ calcCapital <- function(subtype = "Capital") {
     gdpppp= calcOutput("GDPppp", aggregate = F)    
     gdpppp <- mselect(gdpppp, Year=paste0("y",c(2005:2150,5)))
     #getNames(gdpppp) <- sub("gdp_","",getNames(gdpppp))
-    gdpppp <- mselect(gdpppp, variable = c("gdp_SSP1","gdp_SSP2","gdp_SSP3","gdp_SSP4","gdp_SSP5","gdp_SSP2Ariadne"))
+    my_scen <- c("gdp_SSP1", "gdp_SSP2", "gdp_SSP3", "gdp_SSP4", "gdp_SSP5", "gdp_SSP2Ariadne", 
+                 "gdp_SDP", "gdp_SDP_EI", "gdp_SDP_RC", "gdp_SDP_MC",)
+    gdpppp <- mselect(gdpppp, variable = my_scen)
     
     p41 <- setYears(cap_intensity[,rep(1,32),],seq(1995,2150,5))
     p41 <- add_dimension(p41, dim=3.1, add="ssp",nm="ssp1")
-    p41<-setNames(p41[,,rep(1,5)],c("gdp_SSP1","gdp_SSP2","gdp_SSP3","gdp_SSP4","gdp_SSP5","gdp_SSP2Ariadne"))
+    p41<-setNames(p41[,,rep(1,5)], my_scen)
     cap_intensity_future <- p41
     convtime <- p41
     gdp_weight <- p41
