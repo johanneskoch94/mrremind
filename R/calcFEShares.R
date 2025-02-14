c02b1dfec1c14afe218080b11a3c9b630fa6a70d
Johannes Koch <jokoch@pik-potsdam.de>
2025-02-14 14:53:58 +0100

    Fix calcFEShares

--- R/calcFEShares.R
+++ R/calcFEShares.R
@@ -2,28 +2,32 @@
 #'
 #' @param subtype 'ind_coal' for the share of coal used in industry. 'ind_bio' for the share of biomass used in industry
 #'
-#' @author Antoine Levesque
 calcFEShares <- function(subtype) {
-
-  #---- Read Data -----
+  if (! subtype %in% c("ind_coal", "ind_bio", "ind_liq")) {
+    stop("Unknown subtype.")
+  }
 
   edge_buildings <- calcOutput("IOEdgeBuildings", subtype = "output_EDGE_buildings", aggregate = FALSE)
   output <- calcOutput("IO", subtype = "output", aggregate = FALSE)
-  fe_demand <- calcOutput("FEdemand", aggregate = FALSE)[, , "SSP1"]
+  fe_demand <- calcOutput("FEdemand", scenario = "SSP1", aggregate = FALSE)
   fe_demand <- collapseNames(fe_demand)
-
-  #---- Process Data ----
 
   if (subtype == "ind_coal") {
     share <- 1 - dimSums(edge_buildings[, 2005, "coal"]) / output[, 2005, "pecoal.sesofos.coaltr"]
     weight <- output[, 2005, "pecoal.sesofos.coaltr"]
-    descr <- "share of coal used in industry (computed by retrieving buildings uses, i.e., considering transport and ONONSPEC are null)"
-  } else if (subtype == "ind_bio") {
+    descr <- glue::glue("share of coal used in industry (computed by retrieving buildings uses, i.e., considering \\
+                        transport and ONONSPEC are null)")
+  }
+
+  if (subtype == "ind_bio") {
     share <- 1 - dimSums(edge_buildings[, 2005, c("biomod", "biotrad")]) /
       dimSums(output[, 2005, c("pebiolc.sesobio.biotr", "pebiolc.sesobio.biotrmod")], dim = 3)
     weight <- dimSums(output[, 2005, c("pebiolc.sesobio.biotr", "pebiolc.sesobio.biotrmod")], dim = 3)
-    descr <- "share of biomass used in industry (computed by retrieving buildings uses, i.e., considering transport and ONONSPEC are null)"
-  } else if (subtype == "ind_liq") {
+    descr <- glue::glue("share of biomass used in industry (computed by retrieving buildings uses, i.e., considering \\
+                        transport and ONONSPEC are null)")
+  }
+
+  if (subtype == "ind_liq") {
     fehoi <- dimSums(mselect(fe_demand, year = "y2005",
                              item = c("feli_cement", "feli_chemicals", "feli_steel", "feli_otherInd")),
                      dim = 3)
@@ -35,9 +39,9 @@ calcFEShares <- function(subtype) {
   share <- collapseNames(share)
   share[is.na(share)] <- 0
   weight[is.na(weight)] <- 0
-  return(list(
-    x = share, weight = weight,
-    unit = "dimensionless",
-    description = descr
-  ))
+
+  list(x = share,
+       weight = weight,
+       unit = "dimensionless",
+       description = descr)
 }
